39e4a9145326cf5eb6e362d23a0ad922780bbbd2
Vladimir <vlad@quickbird.uk>
2018-05-06 12:31:52 +0200

    Program starts, is avaliable externally. VSCODE settings needed, and that's it!

--- DebugSample/Program.cs
+++ DebugSample/Program.cs
@@ -20,6 +20,8 @@ namespace DebugSample
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseKestrel()
+                .UseUrls("http://*:5000")
                 .Build();
     }
 }
